9cc91e5dfda72db92e0791e1a0230c5a46ef19b0
Yalın Sonat <yalin_sonat@hotmail.com>
2021-10-25 14:31:20 +0300

    Add project files.

---
+++ 2-Add Two Number/2-Add Two Number/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("You can check Palindrome Number. (If they are Palindrome, answer is true)For example, 121 is palindrome while 123 is not.");
+            
+            StartAgain:
+            int y = Convert.ToInt32(Console.ReadLine());
+            Solution solution = new Solution();
+            solution.IsPalindrome(y);
+
+            Console.WriteLine(solution.IsPalindrome(y));
+
+            goto StartAgain;
+            
+
+        }
+        public class Solution
+        {
+
+            public bool IsPalindrome(int x)
+            {
+                if (x < 0)
+                {
+                    return false;
+                }
+
+                int rev = 0;
+                int num = x;
+
+                while (num != 0)
+                {
+                    rev *= 10;
+                    rev += (num % 10);
+                    num /= 10;
+                }
+
+                return (x == rev);
+            }
+        }
+
+
+    }
+}
+
+        
+    
+
---
+++ 3-RomanToInteger/3-RomanToInteger/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_RomanToInteger
+{
+    public class Solution
+    {
+        public int RomanToInt(string s)
+        {
+            int x = 0;
+            int y = 0;
+
+            char[] gelenRakam = s.ToCharArray();
+
+            for (int i = 0; i < gelenRakam.Length; i++)
+            {
+
+                switch (gelenRakam[i])
+                {
+                    case 'M':
+                        y = 1000;
+                        break;
+                    case 'D':
+                        y = 500;
+                        break;
+                    case 'C':
+                        y = 100;
+                        break;
+                    case 'L':
+                        y = 50;
+                        break;
+                    case 'X':
+                        y = 10;
+                        break;
+                    case 'V':
+                        y = 5;
+                        break;
+                    case 'I':
+                        y = 1;
+                        break;
+
+                }
+                x += y;
+            }
+            if (s.Contains("CD") || s.Contains("CM"))
+                x -= 200;
+            if (s.Contains("XL") || s.Contains("XC"))
+                x -= 20;
+            if (s.Contains("IV") || s.Contains("IX"))
+                x -= 2;
+            return x;
+
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string gelen = Console.ReadLine();
+            Solution c = new Solution();
+            int k = c.RomanToInt(gelen);
+            Console.WriteLine(k);
+            Console.ReadKey();
+        }
+    }
+}
