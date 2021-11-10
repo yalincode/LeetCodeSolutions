4077c82c9895fa5f2105facaadf7c71cec2a016b
Yalın Sonat <yalin_sonat@hotmail.com>
2021-11-10 12:12:40 +0300

    new

---
+++ 4-ValidParentheses(20)/4-ValidParentheses(20)/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ValidParentheses_20_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string k = "[[[]";
+            
+            Console.WriteLine(IsValid(k).ToString());
+            Console.ReadLine();
+        }
+
+        public static bool IsValid(string s)
+        {
+            Stack<char> sign = new Stack<char>();
+
+            foreach (var item in s.ToCharArray())
+                if (item == '(')
+                    sign.Push(')');
+                else if (item == '[')
+                    sign.Push(']');
+                else if (item == '{')
+                    sign.Push('}');
+                else if (sign.Count == 0 || sign.Pop() != item)
+                    return false;
+
+            return sign.Count == 0;
+        }
+    }
+}
+//for (int i = 0; i < s.Length - i; i++)
+//{
+//    if (s[i] is '(' && s[s.Length - i - 1] is ')')
+//    {
+//        continue;
+//    }
+//    else if (s[i] is '(' && s[i + 1] is ')')
+//    {
+//        i++;
+//        continue;
+//    }
+//    else if (s[i] is '{' && s[s.Length - i - 1] is '}')
+//    {
+//        continue;
+//    }
+//    else if (s[i] is '{' && s[i + 1] is '}')
+//    {
+//        i++;
+//        continue;
+//    }
+//    else if (s[i] is '[' && s[s.Length - i - 1] is ']')
+//    {
+//        continue;
+//    }
+//    else if (s[i] is '[' && s[i + 1] is ']')
+//    {
+//        i++;
+//        continue;
+//    }
+//    else
+//    {
+//        return false;
+//    }
+//}
+//return true;
+
+//if (s.Length % 2 == 1)
+//{
+//    return false;
+//}
+//else if (s[0] == ')' || s[0] == ']' || s[0] == '}')
+//{
+//    return false;
+//}
+//else
+//{
+//    for (int i = 0; i < s.Length - 1; i++)
+//    {
+//        if (s[i] == '(')
+//        {
+//            if (s[i + 1] == ')')
+//            {
+//                i++;
+//                continue;
+//            }
+
+//            else if ((s.Length - 1 - i) > i && s[s.Length - 1] == ')')
+//            {
+//                continue;
+//            }
+//            else
+//            {
+//                return false;
+//            }
+//        }
+//        else if (s[i] == '[')
+//        {
+//            if (s[i + 1] == ']')
+//            {
+//                i++;
+//                continue;
+//            }
+//            else if ((s.Length - 1 - i) > i && s[s.Length - 1] == ']')
+//            {
+//                continue;
+//            }
+//            else
+//            {
+//                return false;
+//            }
+//        }
+//        else if (s[i] == '{')
+//        {
+//            if (s[i + 1] == '}')
+//            {
+//                i++;
+//                continue;
+//            }
+//            else if ((s.Length - 1 - i) > i && s[s.Length - 1] == '}')
+//            {
+//                continue;
+//            }
+//            else
+//            {
+//                return false;
+//            }
+//        }
+//        else
+//        {
+//            return false;
+//        }
+//    }
+//    return true;
+//}
---
+++ 5-MergeTwoSortedList(21)/5-MergeTwoSortedList(21)/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_MergeTwoSortedList_21_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            var head = new ListNode(-1);
+            var curr = head;//AYNI REFERANSI GÖSTEREN BAŞKA NODE
+
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val <= l2.val)
+                {
+                    curr.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+
+                    curr.next = l2;
+                    l2 = l2.next;
+                }
+
+                curr = curr.next;//ifler içerisinde kullanılan curr.next curr yapılır ve sonraki iterasyonda next node un alınması sağlanır.
+            }
+
+            curr.next = l1 == null ? l2 : l1;
+
+            return head.next;//head in başında dumy node vardı o nodu almadan bir sonraki elemandan geriye döndürüyoruz.
+        }
+
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+    }
+}
